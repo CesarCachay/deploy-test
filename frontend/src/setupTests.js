755a7418edb28c2beab0288429861b788cdea22b
Cesar Cachay <cesar@able.co>
2019-11-20 12:36:33 -0500

    Backend deploy test

--- frontend/src/setupTests.js
+++
@@ -1,7 +0,0 @@
-import { cleanup } from "@testing-library/react";
-import * as emotion from "@emotion/core";
-import { createSerializer } from "jest-emotion";
-
-afterEach(cleanup);
-
-expect.addSnapshotSerializer(createSerializer(emotion));
