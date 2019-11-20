3d4cf3e158f8792091d4acd9da0eeb1dd1953aae
Cesar Cachay <cesar@able.co>
2019-11-20 12:44:29 -0500

    Add build directory of frontend

---
+++ backend/public/build/precache-manifest.fd371f0c0fe408e1539b2a67c69a1a87.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8632793a0b5f74dc5f7f0806bf11b1c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eec45294b2b22e93e02b",
+    "url": "/static/css/2.2c3aa5a0.chunk.css"
+  },
+  {
+    "revision": "eec45294b2b22e93e02b",
+    "url": "/static/js/2.dca9617e.chunk.js"
+  },
+  {
+    "revision": "bfd62d8bfb450a225eca",
+    "url": "/static/js/main.94fe4edb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6e42c6b717e600cd25ac6dcda1046c07",
+    "url": "/static/media/boss.6e42c6b7.png"
+  },
+  {
+    "revision": "e281053d971ac4413b4bb665cebd774a",
+    "url": "/static/media/cloud1.e281053d.svg"
+  },
+  {
+    "revision": "4622556e709c99936a0966245670d8e3",
+    "url": "/static/media/cloud2.4622556e.svg"
+  },
+  {
+    "revision": "2a5c495d34c0eddb4fa092dd8735ff75",
+    "url": "/static/media/cloud3.2a5c495d.svg"
+  },
+  {
+    "revision": "33492793ef632b3e9e744c193024a9f6",
+    "url": "/static/media/cloud4.33492793.svg"
+  },
+  {
+    "revision": "6c5fa35670947f3f9ffa7b6279eb5e34",
+    "url": "/static/media/cloud5.6c5fa356.svg"
+  },
+  {
+    "revision": "b3d083b6b07b22841d2efbf98176fc0d",
+    "url": "/static/media/clouds.b3d083b6.svg"
+  },
+  {
+    "revision": "090864207c4b2cdedce7529319f633e9",
+    "url": "/static/media/codeable.09086420.svg"
+  },
+  {
+    "revision": "d675ddbb3a62bd9577ba44026f6a9c2a",
+    "url": "/static/media/giza-egypt.d675ddbb.svg"
+  },
+  {
+    "revision": "8cc6856cefd1b1594e21c2feadd1f04d",
+    "url": "/static/media/home.8cc6856c.svg"
+  },
+  {
+    "revision": "965d6d1103607a45f4612e5ea4e8bcdd",
+    "url": "/static/media/icon-plane.965d6d11.svg"
+  },
+  {
+    "revision": "fc98abc5ff299d477d1c1c2517f48f43",
+    "url": "/static/media/las-vegas-nevada.fc98abc5.svg"
+  },
+  {
+    "revision": "e88ad790114e7d2a3454a28c6d83543b",
+    "url": "/static/media/london-england.e88ad790.svg"
+  },
+  {
+    "revision": "4663b0e7ddeecfa52d796d2f4926a67b",
+    "url": "/static/media/los-angeles-california.4663b0e7.svg"
+  },
+  {
+    "revision": "11e89df0d045f7c2191cf545c8468f73",
+    "url": "/static/media/new-york.11e89df0.svg"
+  },
+  {
+    "revision": "d1e27aec93c4649ae8739460d502676b",
+    "url": "/static/media/plane.d1e27aec.svg"
+  },
+  {
+    "revision": "16598433b542bc657a9181cbdb744534",
+    "url": "/static/media/rio-de-Janeiro-brazil.16598433.svg"
+  },
+  {
+    "revision": "287cc0245f5c23e0199de4c0146e3332",
+    "url": "/static/media/south-dakota.287cc024.svg"
+  },
+  {
+    "revision": "e25af8748a2c7ab892a7a0e23fe3e3c4",
+    "url": "/static/media/washington-dc.e25af874.svg"
+  }
+]);
